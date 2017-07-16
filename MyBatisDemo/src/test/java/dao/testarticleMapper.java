23443d0e9dab6ad240c606ed771cb152fd5316aa
570221322 <570221322@qq.com>
2017-07-16 23:09:17 +0800

    添加了 插入和更新方法                 手写sql的确方便很多,能做很多

--- MyBatisDemo/src/test/java/dao/testarticleMapper.java
+++ MyBatisDemo/src/test/java/dao/testarticleMapper.java
@@ -61,4 +61,20 @@ public class testarticleMapper {
             System.out.println(author_article.getArticle_content());
         }
     }
+
+    @Test
+    public void test4Insert() {
+        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
+        Article article = articleMapper.selectById(1);
+        articleMapper.insert(article);
+          session.commit();
+    }
+    @Test
+    public void test5Update() {
+        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
+        Article article = articleMapper.selectById(1);
+        article.setArticle_content("测试更新content");
+        articleMapper.update(article);
+        session.commit();
+    }
 }

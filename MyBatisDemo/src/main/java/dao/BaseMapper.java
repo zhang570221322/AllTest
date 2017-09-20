7490cf7373cb9f077f2541c35009ec5b64e038ab
570221322 <570221322@qq.com>
2017-09-20 13:45:51 +0800

    2017年9月20日13:45:47 更新

--- MyBatisDemo/src/main/java/dao/BaseMapper.java
+++ MyBatisDemo/src/main/java/dao/BaseMapper.java
@@ -6,10 +6,10 @@ import java.util.LinkedList;
  * Created by Administrator on 2017/7/16.
  */
 public interface BaseMapper<T> {
-     int insert(T t );
+     int insert(T t);
      int deleteById(int id);
-     int update(T t );
-     T select(T t);
-     T selectById(int id);
-     LinkedList<T> selectAll();
+     int update(T t);
+     T get(T t);
+     T getById(int id);
+     LinkedList<T> listAll();
 }
--- MyBatisDemo/src/test/java/dao/testarticleMapper.java
+++ MyBatisDemo/src/test/java/dao/testarticleMapper.java
@@ -27,55 +27,55 @@ public class testarticleMapper {
 //        MyBatisUtil.colsesqlSession(session);
     }
     //测试获取单个对象 使用 articleMapper1.xml
-    @Test
-    public void test1() {
-        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
-        Article article = articleMapper.selectById(1);
-        System.out.println(article.getArticle_blog());
-
-    }
-    private void printAB(  Blog b1, Blog b2){
-        System.out.println(b1.toString());
-        System.out.println(b2.toString());
-    }
-    private void printAB(  Author b1, Author b2){
-        System.out.println(b1.toString());
-        System.out.println(b2.toString());
-    }
-     //测试集合
-    @Test
-    public void test2() {
-        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
-        LinkedList<Article> articles = articleMapper.selectAll();
-        for (Article article : articles) {
-            System.out.println(article);
-        }
-    }
-
-    @Test
-    public void test3() {
-        AuthorMapper authorMapper = session.getMapper(AuthorMapper.class);
-        Author author = authorMapper.selectById(1);
-        HashSet<Article> author_articles = author.getAuthor_articles();
-        for (Article author_article : author_articles) {
-            System.out.println(author_article.getArticle_content());
-        }
-    }
-
-    @Test
-    public void test4Insert() {
-        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
-        Article article = articleMapper.selectById(1);
-        articleMapper.insert(article);
-          session.commit();
-    }
-    @Test
-    public void test5Update() {
-        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
-
-        Article article = articleMapper.selectById(1);
-        article.setArticle_content("测试更新content");
-        articleMapper.update(article);
-        session.commit();
-    }
+//    @Test
+//    public void test1() {
+//        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
+//        Article article = articleMapper.selectById(1);
+//        System.out.println(article.getArticle_blog());
+//
+//    }
+//    private void printAB(  Blog b1, Blog b2){
+//        System.out.println(b1.toString());
+//        System.out.println(b2.toString());
+//    }
+//    private void printAB(  Author b1, Author b2){
+//        System.out.println(b1.toString());
+//        System.out.println(b2.toString());
+//    }
+//     //测试集合
+//    @Test
+//    public void test2() {
+//        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
+//        LinkedList<Article> articles = articleMapper.selectAll();
+//        for (Article article : articles) {
+//            System.out.println(article);
+//        }
+//    }
+//
+//    @Test
+//    public void test3() {
+//        AuthorMapper authorMapper = session.getMapper(AuthorMapper.class);
+//        Author author = authorMapper.selectById(1);
+//        HashSet<Article> author_articles = author.getAuthor_articles();
+//        for (Article author_article : author_articles) {
+//            System.out.println(author_article.getArticle_content());
+//        }
+//    }
+//
+//    @Test
+//    public void test4Insert() {
+//        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
+//        Article article = articleMapper.selectById(1);
+//        articleMapper.insert(article);
+//          session.commit();
+//    }
+//    @Test
+//    public void test5Update() {
+//        ArticleMapper articleMapper = session.getMapper(ArticleMapper.class);
+//
+//        Article article = articleMapper.selectById(1);
+//        article.setArticle_content("测试更新content");
+//        articleMapper.update(article);
+//        session.commit();
+//    }
 }
